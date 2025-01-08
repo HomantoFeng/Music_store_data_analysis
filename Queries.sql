ce6426bbbd40255d95bfd5e535a50710d434b8da
HomatoFeng <breakwave922@gmail.com>
2025-01-07 21:25:58 -0600

    update query

--- Queries.sql
+++ Queries.sql
@@ -1,1 +1,14 @@
--- Q1: find info of all Rock Music listeners
+-- Q1: find info of all Rock Music listeners returning emails, first name, last name 
+SELECT email,first_name,last_name
+FROM Music_store.invoice
+JOIN Music_store.invoice_line 
+USING (invoice_id)
+JOIN Music_store.customer
+USING (customer_id)
+JOIN Music_store.track
+USING (track_id)
+JOIN Music_store.genre
+USING (genre_id)
+where genre.name='Rock'
+group by email,first_name,last_name
+order by email
