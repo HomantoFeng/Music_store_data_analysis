deac8409344b7dcca91e940658308de2a28c315e
HomatoFeng <breakwave922@gmail.com>
2025-01-12 20:40:21 -0600

    update query

--- Queries.sql
+++ Queries.sql
@@ -34,4 +34,24 @@ LIMIT 10
 SELECT name, milliseconds
 FROM Music_store.track 
 where milliseconds> (SELECT AVG(milliseconds) as ave_len FROM Music_store.track)
+order by milliseconds desc
  
+ -- Q3: find out how much amount spent by each customer on artists? Write a query to return customer name,artist nanme and total spent
+WITH Album_all as (SELECT * FROM Music_store.album UNION SELECT * FROM Music_store.album2)
+
+SELECT * -- first_name,last_name,artist.name,sum(invoice_line.unit_price*invoice_line.quantity) as amount_spent
+FROM Music_store.invoice
+JOIN Music_store.invoice_line 
+USING (invoice_id)
+JOIN Music_store.customer
+USING (customer_id)
+JOIN Music_store.track 
+USING (track_id)
+JOIN Album_all
+USING (Album_id)
+JOIN Music_store.artist
+USING (artist_id)
+where first_name='Johannes' and last_name='Van der Berg'
+-- GROUP BY first_name,last_name,artist.name
+-- HAVING first_name='Johannes'
+-- ORDER BY first_name,last_name,artist.name
